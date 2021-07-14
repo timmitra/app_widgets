7cb2ec05e81e648c97fb5b9b0fb584aa5255c7c2
Tim Mitra <twitter@it-guy.com>
2021-07-14 01:59:23 -0400

    fonts was made in the wrong place. Should have been at root

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -7,16 +7,16 @@ class Home extends StatelessWidget {
         child: Container(
       alignment: Alignment.center,
       color: Colors.deepOrangeAccent,
-      child: Text("Pizza", 
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-        fontSize: 80.0,
-        decoration: TextDecoration.none,
-        fontFamily: 'Oxygen',
-        fontWeight: FontWeight.bold,
+      child: Text(
+        "Pizza",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 80.0,
+          decoration: TextDecoration.none,
+          fontFamily: 'Oxygen',
+          fontWeight: FontWeight.w300,
         ),
       ),
-    )
-    );
+    ));
   }
 }
