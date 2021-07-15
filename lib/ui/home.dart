b949d0de8d71e4545fc2d0ad4101980ed901af0e
Tim Mitra <twitter@it-guy.com>
2021-07-15 16:45:35 -0400

    added rows and colums, and an image

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -4,19 +4,77 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-      alignment: Alignment.center,
-      color: Colors.deepOrangeAccent,
-      child: Text(
-        "Pizza",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 80.0,
-          decoration: TextDecoration.none,
-          fontFamily: 'Oxygen',
-          fontWeight: FontWeight.w300,
+      child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(top: 30.0, left: 10.0),
+        color: Colors.deepOrangeAccent,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                  "Margheritta",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    decoration: TextDecoration.none,
+                    fontFamily: 'Oxygen',
+                    fontWeight: FontWeight.normal,
+                  ),
+                )),
+                Expanded(
+                    child: Text(
+                  "Tomato, Mozzarella, Basil",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    decoration: TextDecoration.none,
+                    fontFamily: 'Oxygen',
+                    fontWeight: FontWeight.normal,
+                  ),
+                )),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                  "Marinara",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    decoration: TextDecoration.none,
+                    fontFamily: 'Oxygen',
+                    fontWeight: FontWeight.w300,
+                  ),
+                )),
+                Expanded(
+                    child: Text(
+                  "Tomato, Garlic",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    decoration: TextDecoration.none,
+                    fontFamily: 'Oxygen',
+                    fontWeight: FontWeight.w300,
+                  ),
+                )),
+              ],
+            ),
+            PizzaImageWidget()
+          ],
         ),
       ),
-    ));
+    );
+  }
+}
+
+class PizzaImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaAsset = AssetImage('images/pizza.png');
+    Image image = Image(image: pizzaAsset, width: 400.0, height: 400.0);
+    return Container(child: image);
   }
 }
